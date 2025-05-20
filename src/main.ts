3e245c8763d9964e9aae628a79df81ab5d182c04
Akinori Nakata <akinori.nakata@bright-century.com>
2025-05-20 18:03:48 +0900

    例外フィルタ & システムログ出力 実装

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import accessLogger from './log/accessLogger';
+import { HttpExceptionFilter } from './http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(accessLogger({ level: 'info' }));
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
